7bc4cc2a428b7e2b589be2851fe18efe0dfc6d2d
Deck Reyes <63816737+deckreyes@users.noreply.github.com>
2024-05-09 13:19:54 -0700

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -28,8 +28,8 @@ fetch(aem + '/graphql/execute.json/aem-demo-assets/adventures-by-slug;slug=' + s
 .then(response => response.json())
 .then(response => {
 
-//const backgroundImage = response.data.adventureList.items[0].primaryImage._publishUrl;
-//document.getElementById(adventureDiv.id).innerHTML = "<section><img src=" + backgroundImage + "></section>";  
+const backgroundImage = response.data.adventureList.items[0].primaryImage._publishUrl;
+document.getElementById(adventureDiv.id).innerHTML = "<section><img src=" + backgroundImage + "></section>";  
 
 const adventureTitle = response.data.adventureList.items[0].title;
 document.getElementById(adventureDiv.id).innerHTML += "<section><h3>"+ adventureTitle + "</h3></section>";
