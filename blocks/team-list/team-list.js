2eacea90e861eb32b07e9a062e2e1aa69fce80c6
deck-adobe <deckreyes@adobe.com>
2024-06-05 15:53:40 -0700

    team list changes

--- blocks/team-list/team-list.js
+++ blocks/team-list/team-list.js
@@ -6,6 +6,10 @@ export default function decorate(block) {
 fetch(AEM_HOST + '/graphql/execute.json/my-project/all-teams')
 .then(response => response.json())
 .then(response => {
+
+    const title = response.data.teamList.items[0].title;
+    document.write (title)
+
 })
 .catch(error => {
   console.log('Error fetching data:', error);
