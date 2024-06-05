54b2799f56718ab17602b952176c8210682ea871
deck-adobe <deckreyes@adobe.com>
2024-06-05 16:12:52 -0700

    team-list updated.

--- blocks/team-list/team-list.js
+++ blocks/team-list/team-list.js
@@ -3,7 +3,7 @@ const AEM_HOST_2 = "https://publish-p131639-e1282833.adobeaemcloud.com";
 
 export default function decorate(block) {
 
-fetch(AEM_HOST_2 + '/graphql/execute.json/my-project/all-teams')
+fetch('https://author-p131639-e1282833.adobeaemcloud.com/graphql/execute.json/my-project/all-teams')
 .then(response => response.json())
 .then(response => {
 
