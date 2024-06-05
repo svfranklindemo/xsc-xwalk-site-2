2524c108acafc980bf17e0dc4aa88c85fd56f5fd
deck-adobe <deckreyes@adobe.com>
2024-06-05 16:11:35 -0700

    team-list updated.

--- blocks/team-list/team-list.js
+++ blocks/team-list/team-list.js
@@ -1,9 +1,9 @@
-//local react host
-const AEM_HOST = "https://publish-p131639-e1282833.adobeaemcloud.com";
+//AEM Publish instance
+const AEM_HOST_2 = "https://publish-p131639-e1282833.adobeaemcloud.com";
 
 export default function decorate(block) {
 
-fetch(AEM_HOST + '/graphql/execute.json/my-project/all-teams')
+fetch(AEM_HOST_2 + '/graphql/execute.json/my-project/all-teams')
 .then(response => response.json())
 .then(response => {
 
